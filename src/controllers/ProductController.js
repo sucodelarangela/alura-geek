2d25e124eb8b980596863e354f09df87e4428763
Angela Caldas <aes.caldas@gmail.com>
2022-03-29 16:06:58 -0300

    Comments on controller

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,8 +1,8 @@
 module.exports = {
   index(req, res) {
-    const itemCode = req.params.code
-    const password = req.body.password
+    const itemCode = req.params.code // gets parameter from form action route variable
+    const password = req.body.password // gets password typed on password input on modal
 
-    console.log(itemCode, password)
+    console.log(itemCode, password) // show data on console for verifying if everything is working
   }
 }
