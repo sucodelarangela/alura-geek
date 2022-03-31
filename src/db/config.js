2850966504b9db7a88724e6d65483fafb6c140f9
Angela Caldas <aes.caldas@gmail.com>
2022-03-31 16:48:22 -0300

    Add database config file

---
+++ src/db/config.js
@@ -0,0 +1,12 @@
+// importing sqlite and sqlite3
+const sqlite3 = require('sqlite3')
+// const sqlite = require('sqlite')
+// even though we can import sqlite as above, we do not need to import all its funcionalities because we're going to use only the "open" function. To import only this function, we declare the funcion name as the const name, but between {}, like this:
+const {open} = require('sqlite')
+
+module.exports = () => {
+  open({
+    filename: '/src/db/geek.sqlite',
+    driver: sqlite3.Database
+  })
+}
